1d8afd1eadd3a97eff690987ce4bebaae0527948
Gandy <gandy0215@naver.com>
2022-12-22 11:18:22 +0900

    로그 저장기능
    누락 파일 추가

---
+++ log-manager/src/test/java/me/gandy/logmanager/LogManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gandy.logmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
